280752836aa105413ebdd7ae754b684f75a2d159
grazianomiglionico <gramiglionico@gmail.com>
2023-02-16 01:04:23 +0100

    [FE] improve UX when show results of election

--- src/democracy_frontend/src/Item.jsx
+++ src/democracy_frontend/src/Item.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
 import {idlFactory} from "../../declarations/election";
 import { Actor, HttpAgent } from "@dfinity/agent";
-import { election } from "../../declarations/election";
-import {Card, CardActions, CardContent, Grid, Typography} from "@mui/material";
-import Button from "@mui/material/Button";
+import {Box, Card, CardActions, CardContent, Grid, Typography} from "@mui/material";
+import LoadingButton from '@mui/lab/LoadingButton';
 
 const Item = (props) => {
   const [name, setName] = useState();
   const [options, setOptions] = useState([]);
+  const [yesVote, setYesVote] = useState('');
+  const [noVote, setNoVote] = useState('');
+
+  const [loading, setLoading] = useState(false);
+  const [shouldDisplay, setDisplay] = useState(false);
 
   const id = props.id;
 
@@ -27,24 +31,33 @@ const Item = (props) => {
     const name = await ElectionActor.getName();
     const options = await ElectionActor.getOptions();
 
-    const yesCount = await ElectionActor.getYes();
-    console.log(name, ' yes: ', yesCount)
-    const noCount = await ElectionActor.getNo();
-    console.log(name, ' no: ', noCount)
-
-    //setName(name);
-    setName(name + ': yes = ' + yesCount + '; no = ' + noCount)
+    setName(name);
     setOptions(options);
-    console.log(options);
+
+  }
+
+  async function loadResults() {
+    ElectionActor = await Actor.createActor(idlFactory,{
+      agent,
+      canisterId: id,
+    });
+
+    const yesCount = await ElectionActor.getYes();
+    const noCount = await ElectionActor.getNo();
+    setYesVote('' + yesCount);
+    setNoVote('' + noCount);
   }
 
   useEffect(() => {
     console.log("[item] useEffect is triggered")
     loadElection();
+    loadResults();
   }, []);
 
   async function handleVote(e, option) {
     e.preventDefault();
+    setLoading(true);
+
     console.log(option);
     ElectionActor = await Actor.createActor(idlFactory,{
       agent,
@@ -53,22 +66,36 @@ const Item = (props) => {
 
     const vote = await ElectionActor.vote(option);
     console.log(vote);
+    setLoading(false);
+
+    loadResults();
+  }
+
+  async function handleResults(e) {
+    e.preventDefault();
+    setDisplay(!shouldDisplay);
   }
 
   return (
     <Grid item xs={3} md={4}>
       <Card sx={{ minWidth: 225 }}>
           <CardContent>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-              label
-            </Typography>
             <Typography variant="h5" component="div">
               {name}
             </Typography>
+            <Box style={{ display: shouldDisplay ? "inline" : "none" }}>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                yes = {yesVote}
+              </Typography>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                no = {noVote}
+              </Typography>
+            </Box>
           </CardContent>
           <CardActions>
-            <Button size="small" onClick={event => handleVote(event, options.at(0))}>{options.at(0)}</Button>
-            <Button size="small" onClick={event => handleVote(event, options.at(1))}>{options.at(1)}</Button>
+            <LoadingButton loading={loading} loadingPosition="start" size="small" onClick={event => handleVote(event, options.at(0))}>{options.at(0)}</LoadingButton>
+            <LoadingButton loading={loading} loadingPosition="start" size="small" onClick={event => handleVote(event, options.at(1))}>{options.at(1)}</LoadingButton>
+            <LoadingButton loading={loading} loadingPosition="start" size="small" onClick={event => handleResults(event)}> <span>{shouldDisplay ? "Hide" : "Show"} results</span> </LoadingButton>
           </CardActions>
         </Card>
     </Grid>
